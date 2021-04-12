cef37cd0ed0314dfbca89c64e5dcadf863f07010
Janus Valberg-Madsen <janusvm@gmail.com>
2021-04-12 07:54:44 +0200

    ✨ Add Colemak-DH layout and homerow mods

--- keyboards/planck/keymaps/janusvm/keymap.c
+++ keyboards/planck/keymaps/janusvm/keymap.c
@@ -1,32 +1,53 @@
+// TODO:
+// * Add a Clip Studio Paint layer
+// * Rethink layers
+//   - Fn layer is uncomfortable to use
+//   - ÆØÅ worked better on Lower layer
+// * Emacs UC mode
+
 #include QMK_KEYBOARD_H
-#include "muse.h"
 #include "unicode.h"
 
 // Layers
 enum planck_layers {
     _QWERTY,
+    _COLEMAK_DH,
     _NUMPAD,
     _SYMBOLS,
     _GREEK,
     _EMOJI,
+    _CSP,
     _LOWER,
     _RAISE,
     _ADJUST
 };
 
-#define KC_EXIT    TO(_QWERTY)
+enum planck_keycodes {
+  QWERTY = SAFE_RANGE,
+  COLEMAK_DH
+};
+
+#define KC_EXIT    KC_NO
 #define KC_LOWER   MO(_LOWER)
 #define KC_RAISE   MO(_RAISE)
-#define KC_NUMPAD  TT(_NUMPAD)
-#define KC_GREEK   TT(_GREEK)
-#define KC_SYMBOLS TT(_SYMBOLS)
-#define KC_EMOJI   TT(_EMOJI)
+#define KC_NUMPAD  OSL(_NUMPAD)
+#define KC_GREEK   OSL(_GREEK)
+#define KC_SYMBOLS OSL(_SYMBOLS)
+#define KC_EMOJI   OSL(_EMOJI)
 
 // Mod-tap keys
 #define KC_CESC  LCTL_T(KC_ESC)
 #define KC_SCAPS LSFT_T(KC_CAPS)
 #define KC_CQUOT RCTL_T(KC_QUOT)
 #define KC_SENT  RSFT_T(KC_ENT)
+#define KC_GA    LGUI_T(KC_A)
+#define KC_AR    LALT_T(KC_R)
+#define KC_CS    LCTL_T(KC_S)
+#define KC_ST    LSFT_T(KC_T)
+#define KC_SN    RSFT_T(KC_N)
+#define KC_CE    RCTL_T(KC_E)
+#define KC_AI    LALT_T(KC_I)
+#define KC_GO    RGUI_T(KC_O)
 
 // Combined keys
 #define KC_C_BSPC LCTL(KC_BSPC)
@@ -39,11 +60,6 @@ enum planck_layers {
 #define KC_C_V    LCTL(KC_V)
 #define KC_C_ALT  LCA(KC_NO)
 
-// TODO: Language toggle buttons
-// TODO: https://emacs.stackexchange.com/questions/55994/unicode-input-from-keyboard-qmk-to-emacs
-#define EN_MODE KC_NO
-#define DA_MODE KC_NO
-
 // Misc keycodes
 #define KC_ KC_TRNS
 #define KC__ KC_NO
@@ -55,8 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = KC_KEYMAP(
         TAB,  Q,      W,   E,    R,     T,   Y,   U,     I,    O,     P,       BSPC,
         CESC, A,      S,   D,    F,     G,   H,   J,     K,    L,     SCLN,    QUOT,
-        LSFT, Z,      X,   C,    V,     B,   N,   M,     COMM, DOT,   SLSH,    SENT,
+        SCAPS, Z,      X,   C,    V,     B,   N,   M,     COMM, DOT,   SLSH,    SENT,
         LGUI, NUMPAD, APP, LALT, LOWER, SPC, SPC, RAISE, RALT, GREEK, SYMBOLS, EMOJI
+    ),
+
+    // Default layer, based on ANSI Colemak-DHm
+    [_COLEMAK_DH] = KC_KEYMAP(
+        TAB,   Q,      W,   F,    P,     B,   J,   L,     U,    Y,     SCLN,    BSPC,
+        CESC,  GA,     AR,  CS,   ST,    G,   M,   SN,    CE,   AI,    GO,      QUOT,
+        SCAPS, Z,      X,   C,    D,     V,   K,   H,     COMM, DOT,   SLSH,    SENT,
+        LGUI,  NUMPAD, APP, LALT, LOWER, SPC, SPC, RAISE, RALT, GREEK, SYMBOLS, EMOJI
     ),
 
     // Lower layer, with a number row, arrow keys, and some shortcuts
@@ -77,10 +101,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Adjust layer, with mouse keys and board settings
     [_ADJUST] = KEYMAP(
-        RESET,   KC_VOLU, KC_BRIU, EN_MODE, RGB_TOG, ___X___, KC_BTN4, KC_BTN1, KC_BTN2, KC_BTN5, ___X___, UC_M_WC,
-        _______, KC_VOLD, KC_BRID, DA_MODE, AU_TOG,  ___X___, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, ___X___, UC_M_MA,
-        _______, KC_PSCR, KC_INS,  KC_CAPS, MU_TOG,  ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, UC_M_LN,
-        _______, _______, KC_EXIT, _______, _______, KC_WHOM, KC_WHOM, _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY
+        RESET,   KC_VOLU, KC_BRIU, QWERTY,     RGB_TOG, ___X___, KC_BTN4, KC_BTN1, KC_BTN2, KC_BTN5, ___X___, UC_M_WC,
+        _______, KC_VOLD, KC_BRID, COLEMAK_DH, AU_TOG,  ___X___, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, ___X___, UC_M_MA,
+        _______, KC_PSCR, KC_INS,  KC_CAPS,    ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, UC_M_LN,
+        _______, _______, KC_EXIT, _______,    _______, KC_WHOM, KC_WHOM, _______, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY
     ),
 
     // Numpad layer, with function keys and a numpad
@@ -88,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         , F1, F2,   F3,  F4,  7, 8, 9, COLN, EQL,  CIRC, ,
         , F5, F6,   F7,  F8,  4, 5, 6, MINS, PLUS, ASTR, ,
         , F9, F10,  F11, F12, 1, 2, 3, ,     ,     ,     ,
-        , ,   EXIT,    ,    ,    0, 0, ,  ,     ,     ,
+        , ,   EXIT, ,    ,    0, 0, ,  ,     _,    _,    _
     ),
 
     // Greek layer, for entering Greek letters
@@ -96,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         , THETA, OMEGA, EPSILON, RHO, TAU,   UPSILON, PSI, IOTA,  OMICRON, PI, ,
         , ALPHA, SIGMA, DELTA,   PHI, GAMMA, ETA,     _,   KAPPA, LAMBDA,  ,   ,
         , ZETA,  XI,    CHI,     _,   BETA,  NU,      MU,  ,      ,        ,   ,
-        , ,      EXIT,  ,        ,    ,      ,        ,    ,      ,        ,
+        , _,     EXIT,  ,        ,    ,      ,        ,    ,      ,        _,  _
     ),
 
     // Symbols layer, for entering special characters
@@ -104,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         , HALF,         SQUARED,      EURO,      REGISTERED, TRADEMARK, JPY,        UNION,      INFTY,    NEQ,         DEGREES,    ,
         , INTERSECTION, SQRT,         PARTIAL,   INTEGRAL,   GRADIENT,  LEFT_ARROW, DOWN_ARROW, UP_ARROW, RIGHT_ARROW, TIMES,      EMPTY_SET,
         , SOLID_BLOCK,  MEDIUM_BLOCK, COPYRIGHT, ELEMENT_OF, GBP,       EN_DASH,    EM_DASH,    NOT,      BULLET,      PLUS_MINUS, ,
-        , ,             EXIT,         ,          ,           ,          ,           ,           ,         ,            ,
+        , _,            EXIT,         ,          ,           ,          ,           ,           ,         _,           ,           _
     ),
 
     // Emoji layer, for inputting my favourite emoji
@@ -113,6 +137,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         SWEAT_SMILE, HEART_EYES, SOB,     WINK_KISS,   RAISED_EYEBROW, UPSIDE_DOWN, GRIMACE, WARNING,  OK_HAND,     BRAIN,    SPARKLES, SWEAT,
         JOY,         ZANY,       PENSIVE, WINK_TONGUE, UNAMUSED,       POOP,        TRIUMPH, NO_ENTRY, CLAP,        B_BUTTON, HUNDRED,  HEARTS,
         SMILE,       PLEADING,   EXIT,    SMIRK,       ,               ,            ,        ,         THUMBS_UP,   PRAY,     FIRE,
+    ),
+
+    // TODO Clip Studio Paint layer, for quick access to my most used tools and shortcuts
+    [_CSP] = KC_KEYMAP(
+       ,,,,,,,,,,,,
+       ,,,,,,,,,,,,
+       ,,,,,,,,,,,,
+       ,,,,,,,,,,,
     )
 };
 
@@ -141,71 +173,27 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
-
-void encoder_update(bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      #ifdef MOUSEKEY_ENABLE
-        tap_code(KC_MS_WH_DOWN);
-      #else
-        tap_code(KC_PGDN);
-      #endif
-    } else {
-      #ifdef MOUSEKEY_ENABLE
-        tap_code(KC_MS_WH_UP);
-      #else
-        tap_code(KC_PGUP);
-      #endif
-    }
-  }
+// Set initial light to be off
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_setrgb(RGB_OFF);
 }
 
-void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
+// Make default layer switching persistent
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case QWERTY:
+        if (record->event.pressed) {
+            set_single_persistent_default_layer(_QWERTY);
         }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    } else {
-        if (muse_counter) {
-            stop_all_notes();
-            muse_counter = 0;
+        return false;
+        break;
+    case COLEMAK_DH:
+        if (record->event.pressed) {
+            set_single_persistent_default_layer(_COLEMAK_DH);
         }
+        return false;
+        break;
     }
-#endif
-}
-
-bool music_mask_user(uint16_t keycode) {
-  switch (keycode) {
-    case KC_RAISE:
-    case KC_LOWER:
-      return false;
-    default:
-      return true;
-  }
+    return true;
 }
